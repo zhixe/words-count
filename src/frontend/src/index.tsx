9e06ea325a0a52ca2682fffff7ad21c4767f554d
Am <amsyar1989@gmail.com>
2025-06-04 00:23:10 +0800

    🚀 Update: some codes

--- src/frontend/src/index.tsx
+++ src/frontend/src/index.tsx
@@ -1,16 +1,15 @@
-import {type ReactDOM, StrictMode} from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import React from 'react';
-import {ConfigProvider} from "antd";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { ConfigProvider } from 'antd';
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-    <React.StrictMode>
+    <StrictMode>
         <ConfigProvider>
             <App />
         </ConfigProvider>
-    </React.StrictMode>
+    </StrictMode>
 );
